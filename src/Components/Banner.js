8ac56082ecb4914e0f780743f3b9cfc3789bbf80
unknown <6sohaib@gmail.com>
2020-07-12 00:57:04 +0500

    after deploy

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -34,21 +34,23 @@ function Banner() {
         backgroundPosition: "center center",
       }}
     >
-      <div className="banner__contents">
-        <h1 className="banner__title">
-          {movie?.title || movie?.name || movie?.original_name}
-        </h1>
+      <div className="banner__contentsparent">
+        <div className="banner__contents">
+          <h1 className="banner__title">
+            {movie?.title || movie?.name || movie?.original_name}
+          </h1>
 
-        <div className="banner__buttons">
-          <button className="banner__button">Play</button>
-          <button className="banner__button">My List</button>
+          <div className="banner__buttons">
+            <button className="banner__button">Play</button>
+            <button className="banner__button">My List</button>
+          </div>
+          <h1 className="banner__description">
+            {truncate(movie?.overview, 250)}
+            {/* 150 */}
+          </h1>
         </div>
-        <h1 className="banner__description">
-          {truncate(movie?.overview, 250)}
-          {/* 150 */}
-        </h1>
+        <div className="banner--fadeBottom" />
       </div>
-      <div className="banner--fadeBottom" />
     </header>
   );
 }
